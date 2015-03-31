7e6d56cee38d57e2bdbecbfabee11033f05d2955
Alexander During <alexanderduring.git@gmail.com>
2015-03-31 20:27:24 +0200

    Added more test cases for instantiations.

--- data/code/test.php
+++ data/code/test.php
@@ -18,7 +18,38 @@ class Foo implements Barable
     }
 }
 
+
+
+class Thing
+{
+    private $anotherThing;
+
+    public function __construct(AnotherThing $anotherThing)
+    {
+        $this->anotherThing = $anotherThing;
+    }
+}
+
+
+
+class AnotherThing
+{
+    public $property;
+}
+
+
+
+// Instantiation in an assignment
 $var = new Foo();
 $var->bar();
 
+// Instantiation in an assignment
 $var2 = new Baz\Foo();
+
+// Instantiation in an array
+$list = array(
+    'instance' => new Foo()
+);
+
+// Instantiation in a function call
+$var3 = new Thing(new AnotherThing());
