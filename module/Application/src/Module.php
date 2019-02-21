686e03b761032f4803de5d9e79c25d2e9a430ef2
Alexander During <alexanderduring.git@gmail.com>
2019-02-21 16:36:20 +0100

    Removed src/Application folder.

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -18,19 +18,6 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
